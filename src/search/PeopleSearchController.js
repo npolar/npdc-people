fa16b7247f50a4d668a5d2cf62788eae27b26a4c
RDuX <ruben.dens@gmail.com>
2015-11-04 14:04:18 +0100

    basic search controller

---
+++ src/search/PeopleSearchController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// @ngInject
+
+var PeopleSearchController = function ($scope, $location, $controller, People, npdcAppConfig) {
+
+  $controller('NpolarBaseController', { $scope: $scope });
+  $scope.resource = People;
+  npdcAppConfig.cardTitle = "People";
+  npdcAppConfig.search.results = {
+    subtitle: "jobtitle/en"
+  };
+
+  let defaults = { limit: "50", sort: "-updated", fields: 'title,id,updated,jobtitle.en' };
+  let invariants = $scope.security.isAuthenticated() ? {} : {} ;
+  let query = Object.assign({}, defaults, invariants);
+
+  let search = function (q) {
+    $scope.search(Object.assign({}, query, q));
+  };
+
+  search(query);
+
+  $scope.$on('$locationChangeSuccess', (event, data) => {
+    search($location.search());
+  });
+
+};
+
+module.exports = PeopleSearchController;
