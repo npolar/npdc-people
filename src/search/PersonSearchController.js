d40588a117a9ec5313740a0779891dd1e7809102
cnrdh <cnrdh@users.noreply.github.com>
2015-12-10 13:37:13 +0100

    Only search current employees

--- src/search/PersonSearchController.js
+++ src/search/PersonSearchController.js
@@ -13,8 +13,12 @@ var PersonSearchController = function ($scope, $location, $controller, Person, n
     }
   };
 
-  let defaults = { limit: "50", sort: "-updated", fields: 'title,id,updated,jobtitle.en', facets: 'workplace,orgtree' };
-  let invariants = $scope.security.isAuthenticated() ? {} : {} ;
+  let defaults = { limit: "50",
+    sort: "-updated",
+    fields: 'title,id,updated,jobtitle.en',
+    facets: 'workplace,orgtree'
+  };
+  let invariants = $scope.security.isAuthenticated() ? {} : { 'filter-currently_employed': true } ;
   let query = Object.assign({}, defaults, invariants);
 
   let search = function (q) {
